73f3de1279134367db10ad2fc6eb914a11ef2950
Marlysson Silva <marlysson5@gmail.com>
2016-06-22 19:58:34 -0300

    Adicionando produtos em uma lista pra poder iterar sobre eles usando menos linhas para inserir na compra, usando for.

--- src/teste/TesteCompra.java
+++ src/teste/TesteCompra.java
@@ -4,19 +4,24 @@ import calcularDesconto.Desconto;
 import modelo.Cliente;
 import modelo.Compra;
 import modelo.Item;
+import java.util.List;
+import java.util.Arrays;
 
 public class TesteCompra {
 
 	public static void main(String[] args) {
 		Cliente cliente = new Cliente("Luiza", 1000);
 		Compra compra = new Compra(cliente);
-		
+
 		Item sabonete = new Item("Sabonete", 10, 1.5);
-		
 		Item cadeira = new Item("Cadeira", 20, 100);
 		
-		compra.adicionarProduto(sabonete);
-		compra.adicionarProduto(cadeira);
+		List itens = Arrays.asList(sabonete,cadeira);
+
+		for (Item item : itens){
+			compra.adicionarProduto(item);
+		}
+			
 		compra.calcularValorTotalDaCompra();
 		
 		System.out.println(compra.getValorTotal());
